b936101cae26346e783054ae6b3885a4e620fa87
yashvardhan599 <yashvardhanpatil75@gmail.com>
2026-01-04 17:15:13 +0530

    [feat]: tool node added into agent graph

--- soql_agent/app/nodes.py
+++ soql_agent/app/nodes.py
@@ -41,3 +41,44 @@ def agent_node(state: SoqlState):
             },
             goto=END
         )
+
+
+def tool_node(state: SoqlState):
+    logger.info(msg="Inside the Tool Node")
+
+    tool_calls = state.get("messages", [])[-1].tool_calls
+
+    for tool_call in tool_calls:
+        tool_name = tool_call["name"]
+        tool_args = tool_call["args"].copy()
+        tool_call_id = tool_call["id"]
+
+        logger.info(msg=f"@tool args `{tool_args}`")
+        logger.debug(f"Invoking tool: {tool_name} with args: {tool_args}")
+
+        tool = agent_tools[tool_name]
+        updated_tool_call_args = {**tool_args, "state": state}
+
+        tool_result = tool.invoke(updated_tool_call_args)
+
+        state.update(tool_result.get("state", {}))
+
+        # if "state" in tool_result:
+        #     state.update(tool_result["state"])
+
+        state["messages"] = state["messages"] + [
+            ToolMessage(
+                content=str(tool_result["response"]),
+                tool_call_id=tool_call_id
+            )
+        ]
+
+    table_info = state.get("table_info", {})
+
+    return Command(
+        update={
+            "messages": state["messages"],
+            "table_info": table_info
+        },
+        goto="agent_node"
+    )
